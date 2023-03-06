fa95fd87cb171c390edf687c23e0753dcd71804e
senan436 <senansahmammadov436@gmail.com>
2023-03-07 03:44:40 +0400

    first commit

---
+++ Task/HomeWork06.03/HomeWork06.03/Program.cs
@@ -0,0 +1,43 @@
+﻿//task4
+//int num = 9347;
+//int Maxdigit = 0;
+
+//while (num > 0)
+//{
+//    int digit = num % 10;
+//    if (digit > Maxdigit)
+//    {
+//        Maxdigit = digit;
+//    }
+//    num = num / 10;
+//}
+
+//Console.WriteLine("En boyuk reqem: " + Maxdigit);
+
+
+
+
+//task3
+
+int num = 4; 
+int quvvet = 2; 
+
+while (num % quvvet == 0)
+{
+    num /= quvvet;
+}
+
+if (num == 1)
+{
+    Console.WriteLine("2nin quvvetidir");
+}
+else
+{
+    Console.WriteLine("2nin quvveti deyil");
+}
+
+
+
+
+
+
